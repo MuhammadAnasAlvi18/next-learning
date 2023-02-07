7705f8433738be7c1a3c902f19b958dcb49e7ffd
Anas Alvi <81664171+MuhammadAnasAlvi18@users.noreply.github.com>
2023-02-07 15:47:43 +0500

    update jokes.js

--- pages/jokes.js
+++ pages/jokes.js
@@ -7,6 +7,7 @@ const Product = () => {
    const [jokes, setJokes] = useState(null);
 
    const getJokes = async ()=>{
+    setJokes(null);
 
     const res = await fetch("https://official-joke-api.appspot.com/random_joke");
     const data = await res.json();
@@ -18,8 +19,8 @@ const Product = () => {
     <div className="mt-5 pt-3">
       <Header />
       <h2 className="text-center pt-5">RANDOM JOKES GENERATOR</h2>
-      <h3 className="pt-3 text-center">{jokes ? jokes.setup : "Loading"}</h3>
-      <h5 className="text-center">{jokes ? jokes.punchline : "Loading"}</h5>
+      <h3 className="pt-3 text-center">{jokes ? jokes.setup : "Loading ..."}</h3>
+      <h5 className="text-center">{jokes ? jokes.punchline : "Loading ..."}</h5>
       <Link href="#" className="bg_btn" onClick={getJokes}>Get Jokes</Link>
     </div>
   )
